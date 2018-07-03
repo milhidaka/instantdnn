41946602a952f60712d7ca474c3f9941060151ef
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2018-07-03 14:17:35 +0900

    classification from camera

--- resource/assets/index.js
+++ resource/assets/index.js
@@ -1,4 +1,5 @@
 let idnn = undefined;
+let camera_enabled = false;
 
 window.onload = async () => {
     idnn = new InstantDNN();
@@ -11,11 +12,39 @@ window.onload = async () => {
     }
 
     document.getElementById("start_classify").onclick = async () => {
+        let begin = Date.now();
         let probs = await idnn.classify_from_canvas(document.getElementById('target'));
+        let end = Date.now();
         let result = "";
         for (let i = 0; i < probs.length; i++) {
             result += `${idnn.labels[i]}: ${probs[i] * 100 | 0}% `;
         }
+        result += `(${end - begin}ms)`;
         document.getElementById("result").innerText = result;
+    }
+
+    document.getElementById("start_camera").onclick = async () => {
+        navigator.getUserMedia({ audio: false, video: { width: 227, height: 227 } },
+            function(stream) {
+               var video = document.getElementById('camera');
+               video.src = window.URL.createObjectURL(stream);
+               video.onloadedmetadata = function(e) {
+                 video.play();
+                 camera_enabled = true;
+                 setInterval(classify_from_camera, 1000);
+               };
+            },
+            function(err) {
+               console.log("The following error occurred: " + err.name);
+            }
+         );
+    }
+
+    let classify_from_camera = () => {
+        var video = document.getElementById("camera");
+        var canvas = document.getElementById("target");
+        var context = canvas.getContext("2d");
+        context.drawImage(video, 0, 0, 227, 227);
+        document.getElementById("start_classify").click();
     }
 }
