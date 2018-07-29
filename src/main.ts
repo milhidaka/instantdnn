99527c6f939c1ba9db75f3ce18ce0c646a8fc284
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2018-07-29 17:03:38 +0900

    batch feature extraction

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 // Modules to control application life and create native browser window
-import {app, BrowserWindow, ipcMain} from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import * as fs from "fs";
+import { resolve } from "path";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow: BrowserWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadFile('static/index.html')
@@ -55,4 +56,49 @@ ipcMain.on("load-image", (event: any, path: string) => {
   let image_base64 = "data:image/jpeg;base64," + image_rawdata.toString("base64");
   event.sender.send("image-loaded", image_base64);
   console.log("sent image");
+});
+
+// better design pattern?
+let extract_feature_expected_key: number;
+let extract_feature_resolve: Function;
+let extract_feature_reject: Function;
+ipcMain.on("extracted-feature", (event: any, image_key: number, features: number[][]) => {
+  if (image_key === extract_feature_expected_key) {
+    extract_feature_resolve(features);
+  } else {
+    extract_feature_reject(`image key mismatch: ${image_key} !== ${extract_feature_expected_key}`);
+  }
+});
+async function extract_feature(image_key: number, image_base64: string): Promise<number[][]> {
+  mainWindow.webContents.send("extract-feature", image_key, image_base64);
+  return new Promise<number[][]>((resolve, reject) => {
+    extract_feature_expected_key = image_key;
+    extract_feature_resolve = resolve;
+    extract_feature_reject = reject;
+  });
+}
+
+let work_root = "/Users/hidaka/miljs/idnn/work1"
+ipcMain.on("batch-extraction", async (event: any) => {
+  console.log("batch extraction start");
+  // entrypoint of batch feature extraction
+  let files_data = JSON.parse(fs.readFileSync(work_root + "/files.json", "utf8"));
+  let all_features = [];
+  let feature_imgids = [];
+  for (let imgid = 0; imgid < files_data.imgid2path.length; imgid++) {
+    let imgpath = files_data.imgid2path[imgid];
+    console.log(imgpath);
+    let image_rawdata = fs.readFileSync(work_root + "/" + imgpath);
+    let image_base64 = "data:image/jpeg;base64," + image_rawdata.toString("base64");
+    let features = await extract_feature(imgid, image_base64);
+    for (let i = 0; i < features.length; i++) {
+      all_features.push(features[i]);
+      feature_imgids.push(imgid);
+    }
+  }
+  fs.writeFileSync(work_root + "/feature_imgids.json",
+    JSON.stringify(feature_imgids), "utf8");
+  fs.writeFileSync(work_root + "/features.json",
+    JSON.stringify(all_features), "utf8");
+    console.log("batch extraction finished");
 });
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,9 @@
 import { ipcRenderer } from "electron";
+import { resolve } from "url";
 declare const WebDNN: any;
 let runner: any;
+const raw_image_size = 256;
+const dnn_image_size = 227;
 
 async function load_webdnn() {
   try {
@@ -13,20 +16,48 @@ async function load_webdnn() {
 
 load_webdnn();
 
+function set_image(image_base64: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    let canvas = document.getElementById("prepare-image") as HTMLCanvasElement;
+    let ctx = canvas.getContext("2d");
+    let img = new Image();
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, raw_image_size, raw_image_size);
+      resolve();
+    }
+    img.src = image_base64;
+  });
+}
+
 function augmentimage() {
   let prep_canvas = document.getElementById("prepare-image") as HTMLCanvasElement;
-  let left = Math.random() * (256 - 224 + 1) | 0;
-  let top = Math.random() * (256 - 224 + 1) | 0;
+  let left = Math.random() * (raw_image_size - dnn_image_size + 1) | 0;
+  let top = Math.random() * (raw_image_size - dnn_image_size + 1) | 0;
   let mirror = Math.random() >= 0.5;
   let dst_canvas = document.getElementById("dnn-input-image") as HTMLCanvasElement;
   let dst_ctx = dst_canvas.getContext("2d");
   dst_ctx.save();
   if (mirror) {
-    dst_ctx.translate(224, 0);
+    dst_ctx.translate(dnn_image_size, 0);
     dst_ctx.scale(-1, 1);
   }
-  dst_ctx.drawImage(prep_canvas, left, top, 224, 224, 0, 0, 224, 224);
+  dst_ctx.drawImage(prep_canvas, left, top, dnn_image_size, dnn_image_size, 0, 0, dnn_image_size, dnn_image_size);
   dst_ctx.restore();
+}
+
+async function extract_feature(): Promise<Float32Array> {
+  let image = runner.inputs[0]; 
+  let feat = runner.outputs[0];
+  image.set(await WebDNN.Image.getImageArray(document.getElementById("dnn-input-image") as HTMLCanvasElement,{
+    order: WebDNN.Image.Order.CHW,
+    color: WebDNN.Image.Color.BGR,
+    bias: [123.68, 116.779, 103.939]
+  }));
+
+  // Run
+  await runner.run(); 
+
+  return feat.toActual();
 }
 
 window.addEventListener("load", (event) => {
@@ -38,23 +69,28 @@ window.addEventListener("load", (event) => {
     augmentimage();
   };
   document.getElementById("extractfeat").onclick = async () => {
-    let image = runner.inputs[0]; 
-    let feat = runner.outputs[0];
-    image.set(await WebDNN.Image.getImageArray(document.getElementById("dnn-input-image") as HTMLCanvasElement));
-
-    // Run
-    await runner.run(); 
-
-    alert("feat: " + feat.toActual()[0]);
+    let feat = await extract_feature();
+    alert(`feat[0] = ${feat[0]}`);
+  };
+  document.getElementById("batchextract").onclick = () => {
+    // start batch extraction
+    ipcRenderer.send("batch-extraction");
   };
 });
 
 ipcRenderer.on("image-loaded", (event: any, image_base64: string) => {
-  let canvas = document.getElementById("prepare-image") as HTMLCanvasElement;
-  let ctx = canvas.getContext("2d");
-  let img = new Image();
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0, 256, 256);
-  }
-  img.src = image_base64;
+  set_image(image_base64);
 });
+
+ipcRenderer.on("extract-feature", async (event: any, image_key: any, image_base64: string) => {
+  await set_image(image_base64);
+  let feats = [];
+  for (let i = 0; i < 10; i++) {
+    augmentimage();
+    let feat = await extract_feature();
+    feats.push(Array.from(feat));
+  }
+  
+  ipcRenderer.send("extracted-feature", image_key, feats);
+});
+
